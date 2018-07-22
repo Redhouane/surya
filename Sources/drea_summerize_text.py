2227be17a0f514649ec0dab318e39aa5d17ae483
Red1 <redhouane.a@gmail.com>
2018-07-22 22:55:58 +0200

    Summary of our own article available. Enrichment of paper class with a _get_paper_text method for extracting article's text to summarize.

--- Sources/drea_summerize_text.py
+++ Sources/drea_summerize_text.py
@@ -1,3 +1,29 @@
 #!/Users/redhouaneabdellaoui/anaconda/envs/DrEA/bin/python python
 # -*- coding: utf-8 -*-
 
+
+from DrEA_head.DrEA_context import *
+from DrEA_head.DrEA_paper_class import *
+from DrEA_head.DrEA_basic_operations import _opj
+
+import glob
+from pyteaser import Summarize
+
+
+# Upload list of articles to parse
+articles_to_parse = glob.glob(_opj(PARSED_PAPERS_DIRECTORY, '*_parsed.json'))
+
+# Initialization of paper object with article data and metadata
+# See paper class method _read_paper()
+
+abdellaoui_et_all_2018 = paper()
+abdellaoui_et_all_2018 = abdellaoui_et_all_2018._read_paper(articles_to_parse[0])
+
+# Summary is constituted using key phrases concatenation
+key_phrases = Summarize(abdellaoui_et_all_2018.Title,
+                        abdellaoui_et_all_2018._get_paper_text())
+
+summary = '\n'.join(key_phrases)
+
+print('\n')
+print(summary)
